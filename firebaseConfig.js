4f89ca3e780f19ccf2995d115d1d3936a362ad00
ABOULFATH-ZAKARIAE <aboulfath.zakariae@gmail.com>
2021-05-11 23:48:42 +0000

    v1

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA9Ndd9cEaKC64028sSf0gMLbEgn_5hP20",
+  authDomain: "forexcurrency-ade6e.firebaseapp.com",
+  projectId: "forexcurrency-ade6e",
+  storageBucket: "forexcurrency-ade6e.appspot.com",
+  messagingSenderId: "763885473231",
+  appId: "1:763885473231:web:015966eef31372da1696ba",
+  measurementId: "G-XK3M0EDGGS"
+};
